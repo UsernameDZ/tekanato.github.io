afa61147ec65a8da432470df57469b9b2981ca11
Hleb (Bread) <dima82509@gmail.com>
2025-07-17 17:33:48 +0200

    web version 1.3: random buttons

--- myWeb.js
+++ myWeb.js
@@ -1,5 +1,4 @@
 const icon = document.querySelector('.theme_icon');
-
 function theme() {
     if (icon.classList.contains('fa-moon')) {
         icon.classList.remove('fa-moon');
@@ -8,4 +7,84 @@ function theme() {
         icon.classList.remove('fa-sun');
         icon.classList.add('fa-moon');
     }
+}
+
+
+function playSound1() {
+    const audio = new Audio('sounds/among-us-roundstart.mp3');
+    audio.play();
+}
+
+
+const title = document.querySelector('.header_title');
+function changeTitle() {
+    const titles = [
+        "Сайт от Хлеба",
+        "ALTERGAN",
+        "Тут много пасхалок",
+        "do re mi do re do",
+        "random()",
+        "Тут игры будут",
+        "Сайт для развлечения",
+        "Tetoris",
+        "Don't belive in T",
+        " ",
+        "Тут будет много всякой фигни",
+        "Ghost and Pals",
+        "--- -.- .- -.-",
+        "𝕜𝕚𝕣𝕚𝕖𝕤𝕙𝕜𝕚"
+    ];
+    const randomTitle = Math.floor(Math.random() * titles.length);
+    title.textContent = titles[randomTitle];
+}
+
+
+const bodyColor = document.querySelector('body');
+function randomColorBody() {
+    const colors = [
+        '#FF5733',
+        '#33FF57',  
+        '#3357FF',  
+        '#F1C40F',  
+        '#8E44AD', 
+        '#E67E22', 
+        '#2ECC71', 
+        '#3498DB',
+        '#141010',
+    ];
+    const randomColor = Math.floor(Math.random() * colors.length);
+    bodyColor.style.backgroundColor = colors[randomColor];
+}
+
+
+function playSound2() {
+    const audio = new Audio('sounds/deathPaletteSong.mp3');
+    audio.play();
+}
+
+
+const headerColor = document.querySelector('header');
+const imageChange = document.getElementById('catimg');
+function changeAll() {
+    theme();
+    playSound1();
+    changeTitle();
+    randomColorBody();
+    playSound2();
+
+    const colors = [
+        '#FF5733',
+        '#33FF57',  
+        '#3357FF',  
+        '#F1C40F',  
+        '#8E44AD', 
+        '#E67E22', 
+        '#2ECC71', 
+        '#3498DB',
+        '#141010',
+    ];
+    const randomColor = Math.floor(Math.random() * colors.length);
+    headerColor.style.backgroundColor = colors[randomColor];
+
+    imageChange.src = "images/deathPaletteImg1.jpg";
 }
